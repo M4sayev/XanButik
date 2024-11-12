a08d5cb4d12a8aadff4478cbd94ab7371cfbe561
Elvin Musayev <msayev02@gmail.com>
2024-11-12 19:09:17 +0400

    start-off

---
+++ src/assets/assets.js
@@ -0,0 +1,560 @@
+// Logo 
+
+import logo_black_bg from "./logo_black_bg.jpg";
+import logo_sheki_bg from "./logo_sheki_bg.jpg";
+import logo_no_bg from "./logo_no_bg.png";
+
+// -----Home Page-----
+
+import calvin_klein_sweatshirt from "./header-imgs/calvin_klein_sweatshirt.jpg";
+import calvin_klein_sweatshirt_two from "./header-imgs/calvin_klein_sweatshirt_two.jpg";
+import genuine_leather_dress_shoes_one from "./header-imgs/genuine_leather_dress_shoes_one.jpg";
+import half_classic_genuine_leather_dress_shoes from "./header-imgs/half_classic_genuine_leather_dress_shoes.jpg";
+import lv_tracksuit from "./header-imgs/lv_tracksuit.jpg";
+
+import header_image from "./header-imgs/header_image.jpg";
+import shirts_front from "./header-imgs/shirts_front.jpg";
+import shirts_img from "./header-imgs/shirts_img.jpg";
+import tie_image from "./header-imgs/tie_image.jpg";
+
+// TOP
+
+//// Tshirts
+
+////// tshirts single img
+import white_polo_sport from "./top/tshirts/white_polo_sport.jpg";
+import mado_white_collar_black_tshirt from "./top/tshirts/mado_white_collar_black_tshirt.jpg";
+import mado_two_color_white_paisley_tshirt from "./top/tshirts/mado_two_color_white_paisley_tshirt.jpg";
+import mado_two_color_lightbrown_creamy_tshirt from "./top/tshirts/mado_two_color_lightbrown_creamy_tshirt.jpg";
+import mado_murkyblue_tshirt from "./top/tshirts/mado_murkyblue_tshirt.jpg";
+import hog_tshirt_darkgray from "./top/tshirts/hog_tshirt_darkgray.jpg";
+
+////// 4 color polos
+import four_color_polo_tshirts from "./top/tshirts/4-color-polo-tshirts/four_color_polo_tshirts.jpg";
+import four_color_polo_white_polo from "./top/tshirts/4-color-polo-tshirts/four_color_polo_white_polo.jpg";
+
+////// 4 color plain tshirts
+import four_color_plain_tshirts from "./top/tshirts/4-color-plain-tshirts/four_color_plain_tshirts.jpg";
+
+
+//// Long-sleeve
+
+////// single img clothes
+import dark_blue_zipup_sweater_logo from "./top/long-sleeve/dark_blue_zipup_sweater_logo.jpg";
+import five_color_linen_shirts from "./top/long-sleeve/five_color_linen_shirts.jpg";
+import turant_lightbrown_polo_sweater from "./top/long-sleeve/turant_lightbrown_polo_sweater.jpg";
+
+////// zipup-sweater-rx
+import zipup_sweater_rx_one from "./top/long-sleeve/zipup-sweater-rx/zipup_sweater_rx_one.jpg";
+import zipup_sweater_rx_two from "./top/long-sleeve/zipup-sweater-rx/zipup_sweater_rx_two.jpg";
+
+////// sweater-three-color
+import sweater_three_color_one from "./top/long-sleeve/sweater-three-color/sweater_three_color_one.jpg";
+import sweater_three_color_two from "./top/long-sleeve/sweater-three-color/sweater_three_color_two.jpg";
+import sweater_three_color_three from "./top/long-sleeve/sweater-three-color/sweater_three_color_three.jpg";
+import sweater_three_color_four from "./top/long-sleeve/sweater-three-color/sweater_three_color_four.jpg";
+
+////// cardigan
+import cardigan_one from "./top/long-sleeve/cardigan/cardigan_one.jpg";
+import cardigan_two from "./top/long-sleeve/cardigan/cardigan_two.jpg";
+import cardigan_three from "./top/long-sleeve/cardigan/cardigan_three.jpg";
+
+////// 6 color turtleneck
+import six_color_turtleneck_one from "./top/long-sleeve/6-color-turtleneck/six_color_turtleneck_one.jpg";
+import gray_turtleneck from "./top/long-sleeve/6-color-turtleneck/gray_turtleneck.jpg";
+import lightbrown_turtleneck from "./top/long-sleeve/6-color-turtleneck/lightbrown_turtleneck.jpg";
+import white_turtleneck from "./top/long-sleeve/6-color-turtleneck/white_turtleneck.jpg";
+
+
+//// Jackets 
+
+////// black-down-jacket
+import black_down_jacket_one from "./top/jackets/black-down-jacket/black_down_jacket_one.jpg";
+import black_down_jacket_two from "./top/jackets/black-down-jacket/black_down_jacket_two.jpg";
+import black_down_jacket_three from "./top/jackets/black-down-jacket/black_down_jacket_three.jpg";
+
+////// omon faux leather jacket
+import omon_faux_leather_jacket_one from "./top/jackets/omon-faux-leather-jacket/omon_faux_leather_jacket_one.jpg";
+import omon_faux_leather_jacket_two from "./top/jackets/omon-faux-leather-jacket/omon_faux_leather_jacket_two.jpg";
+import omon_faux_leather_jacket_three from "./top/jackets/omon-faux-leather-jacket/omon_faux_leather_jacket_three.jpg";
+
+////// terra black puffer
+import terra_black_puffer_jacket_one from "./top/jackets/terra-black-puffer-jacket/terra_black_puffer_jacket_one.jpg";
+import terra_black_puffer_jacket_two from "./top/jackets/terra-black-puffer-jacket/terra_black_puffer_jacket_two.jpg";
+import terra_black_puffer_jacket_three from "./top/jackets/terra-black-puffer-jacket/terra_black_puffer_jacket_three.jpg";
+
+
+// BOTTOM
+
+//// Classic
+
+////// three color drawstring pants
+import black_drawstring_pants from "./bottom/classic/three_color_drawstring_pants/black_drawstring_pants.jpg";
+import blue_drawstring_pants from "./bottom/classic/three_color_drawstring_pants/blue_drawstring_pants.jpg";
+import lightdark_drawstring_pants from "./bottom/classic/three_color_drawstring_pants/lightdark_drawstring_pants.jpg";
+import three_color_drawstring_pants_one from "./bottom/classic/three_color_drawstring_pants/three_color_drawstring_pants_one.jpg";
+
+//// Jeans
+
+////// armani gray-blue jeans
+import armani_gray_blue_jeans_one from "./bottom/jeans/armani-gray-blue-jeans/armani_gray_blue_jeans_one.jpg";
+import armani_gray_blue_jeans_two from "./bottom/jeans/armani-gray-blue-jeans/armani_gray_blue_jeans_two.jpg";
+
+////// boyfriend light-blue jeans
+import boyfriend_light_blue_jeans_one from "./bottom/jeans/boyfriend-light-blue-jeans/boyfriend_light_blue_jeans_one.jpg";
+import boyfriend_light_blue_jeans_two from "./bottom/jeans/boyfriend-light-blue-jeans/boyfriend_light_blue_jeans_two.jpg";
+
+////// designs dark gray jeans
+import designs_dark_gray_jeans_one from "./bottom/jeans/designs-dark-gray-jeans/designs_dark_gray_jeans_one.jpg";
+import designs_dark_gray_jeans_two from "./bottom/jeans/designs-dark-gray-jeans/designs_dark_gray_jeans_two.jpg";
+
+////// elastic waist cargo pants 
+import elastic_waist_black_cargo_pants_one from "./bottom/jeans/elastic-waist-cargo-pants/elastic_waist_black_cargo_pants_one.jpg";
+import elastic_waist_black_cargo_pants_two from "./bottom/jeans/elastic-waist-cargo-pants/elastic_waist_black_cargo_pants_two.jpg";
+
+////// paris murky blue jeans
+import paris_murky_blue_jeans_one from "./bottom/jeans/paris-murky-blue-jeans/paris_murky_blue_jeans_one.jpg";
+import paris_murky_blue_jeans_two from "./bottom/jeans/paris-murky-blue-jeans/paris_murky_blue_jeans_two.jpg";
+
+
+// SHOES
+
+////// single img shoes
+import black_loafers_white_soles from "./assets/shoes/black_loafers_white_soles.jpg";
+import creamy_n_sneakers from "./assets/shoes/creamy_n_sneakers.jpg";
+import dark_brown_sneakers from "./assets/shoes/dark_brown_sneakers.jpg";
+import dolumdia_sneakers from "./assets/shoes/dolumdia_sneakers.jpg";
+import double_stripe_sneakers from "./assets/shoes/double_stripe_sneakers.jpg";
+import mesh_black_loafers from "./assets/shoes/mesh_black_loafers.jpg";
+import mesh_gray_sneakers from "./assets/shoes/mesh_gray_sneakers.jpg";
+
+////// black brown sole sneakers
+import black_brown_sole_sneakers_one from "./assets/shoes/black-brown-sole-sneakers/black_brown_sole_sneakers_one.jpg";
+import black_brown_sole_sneakers_two from "./assets/shoes/black-brown-sole-sneakers/black_brown_sole_sneakers_two.jpg";
+import black_brown_sole_sneakers_three from "./assets/shoes/black-brown-sole-sneakers/black_brown_sole_sneakers_three.jpg";
+
+////// fuzzy ankle boots
+import fuzzy_ankle_boots_one from "./assets/shoes/fuzzy-ankle-boots/fuzzy_ankle_boots_one.jpg";
+import fuzzy_ankle_boots_two from "./assets/shoes/fuzzy-ankle-boots/fuzzy_ankle_boots_two.jpg";
+
+////// mustard color loafers
+import mustard_color_loafers_one from "./assets/shoes/mustard-color-loafers/mustard_color_loafers_one.jpg";
+import mustard_color_loafers_two from "./assets/shoes/mustard-color-loafers/mustard_color_loafers_two.jpg";
+
+export const items_list = [
+    {
+        id: 1,
+        name: "Black Down Jacket",
+        price: 65,
+        imgs: {
+            black_down_jacket_one,
+            black_down_jacket_two,
+            black_down_jacket_three
+        },
+        category: "Jacket & Coats",
+        description: "coming soon..."
+    },
+    {
+        id: 2,
+        name: "Black Leather Jacket",
+        price: 65,
+        imgs: {
+            omon_faux_leather_jacket_one,
+            omon_faux_leather_jacket_two,
+            omon_faux_leather_jacket_three
+        },
+        category: "Jacket & Coats",
+        description: "coming soon..."
+    },
+    {
+        id: 3,
+        name: "Black Puffer Jacket",
+        price: 65,
+        imgs: {
+            terra_black_puffer_jacket_one,
+            terra_black_puffer_jacket_two,
+            terra_black_puffer_jacket_three
+        },
+        category: "Jacket & Coats",
+        description: "coming soon..."
+    },
+    {
+        id: 4,
+        name: "Turtleneck sweater",
+        price: 35,
+        imgs: {
+            gray_turtleneck,
+            lightbrown_turtleneck,
+            six_color_turtleneck_one,
+            white_turtleneck
+        },
+        category: "Sweaters & Cardigans",
+        description: "coming soon..."
+    },
+    {
+        id: 5,
+        name: "Zip-up Cardigan",
+        price: 35,
+        imgs: {
+           cardigan_one,
+           cardigan_two,
+           cardigan_three
+        },
+        category: "Sweaters & Cardigans",
+        description: "coming soon..."
+    },
+    {
+        id: 6,
+        name: "Crewneck Sweater",
+        price: 45,
+        imgs: {
+           sweater_three_color_one,
+           sweater_three_color_two,
+           sweater_three_color_three,
+           sweater_three_color_four
+        },
+        category: "Sweaters & Cardigans",
+        description: "coming soon..."
+    },
+    {
+        id: 7,
+        name: "Zip-up Sweater",
+        price: 45,
+        imgs: {
+           zipup_sweater_rx_one,
+           zipup_sweater_rx_two
+        },
+        category: "Sweaters & Cardigans",
+        description: "coming soon..."
+    },
+    {
+        id: 8,
+        name: "Dark Blue Zip-up Sweater",
+        price: 45,
+        imgs: {
+           dark_blue_zipup_sweater_logo
+        },
+        category: "Sweaters & Cardigans",
+        description: "coming soon..."
+    },
+    {
+        id: 9,
+        name: "Linen Shirt",
+        price: 25,
+        imgs: {
+           five_color_linen_shirts
+        },
+        category: "Shirts",
+        description: "coming soon..."
+    },
+    {
+        id: 10,
+        name: "Lightbrown Polo Sweater",
+        price: 45,
+        imgs: {
+           turant_lightbrown_polo_sweater
+        },
+        category: "Sweaters & Cardigans",
+        description: "coming soon..."
+    },
+    {
+        id: 11,
+        name: "Plain T-Shirt",
+        price: 25,
+        imgs: {
+           four_color_plain_tshirts
+        },
+        category: "T-Shirts",
+        description: "coming soon..."
+    },
+    {
+        id: 12,
+        name: "Polo T-Shirt",
+        price: 25,
+        imgs: {
+           four_color_polo_tshirts,
+           four_color_polo_white_polo
+        },
+        category: "T-Shirts",
+        description: "coming soon..."
+    },
+    {
+        id: 13,
+        name: "Darkgray Hog T-Shirt",
+        price: 25,
+        imgs: {
+           hog_tshirt_darkgray
+        },
+        category: "T-Shirts",
+        description: "coming soon..."
+    },
+    {
+        id: 14,
+        name: "Mado Blue T-Shirt",
+        price: 25,
+        imgs: {
+           mado_murkyblue_tshirt
+        },
+        category: "T-Shirts",
+        description: "coming soon..."
+    },
+    {
+        id: 15,
+        name: "Mado Lightbrown-Creamy T-Shirt",
+        price: 25,
+        imgs: {
+           mado_two_color_lightbrown_creamy_tshirt
+        },
+        category: "T-Shirts",
+        description: "coming soon..."
+    },
+    {
+        id: 16,
+        name: "Mado White-Paisley T-Shirt",
+        price: 25,
+        imgs: {
+           mado_two_color_white_paisley_tshirt
+        },
+        category: "T-Shirts",
+        description: "coming soon..."
+    },
+    {
+        id: 17,
+        name: "Mado Black n' White T-Shirt",
+        price: 25,
+        imgs: {
+           mado_white_collar_black_tshirt
+        },
+        category: "T-Shirts",
+        description: "coming soon..."
+    },
+    {
+        id: 18,
+        name: "White Sport Polo",
+        price: 25,
+        imgs: {
+           white_polo_sport
+        },
+        category: "T-Shirts",
+        description: "coming soon..."
+    },
+    {
+        id: 19,
+        name: "Black Drawstring Classic Pants",
+        price: 35,
+        img: {
+            black_drawstring_pants
+        },
+        category: "Classic Pants",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 20,
+        name: "Blue Drawstring Classic Pants",
+        price: 35,
+        img: {
+            blue_drawstring_pants
+        },
+        category: "Classic Pants",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 21,
+        name: "Blackish Drawstring Classic Pants",
+        price: 35,
+        img: {
+            lightdark_drawstring_pants
+        },
+        category: "Classic Pants",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 22,
+        name: "Drawstring Classic Pants",
+        price: 35,
+        img: {
+            three_color_drawstring_pants_one
+        },
+        category: "Classic Pants",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 23,
+        name: "Armani Gray Blue-gray Jeans",
+        price: 45,
+        img: {
+            armani_gray_blue_jeans_one,
+            armani_gray_blue_jeans_two
+        },
+        category: "Jeans",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 24,
+        name: "Light Blue Boyfriend Jeans",
+        price: 45,
+        img: {
+            boyfriend_light_blue_jeans_one,
+            boyfriend_light_blue_jeans_two
+        },
+        category: "Jeans",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 25,
+        name: "Designs Dark Gray Jeans",
+        price: 45,
+        img: {
+            designs_dark_gray_jeans_one,
+            designs_dark_gray_jeans_two
+        },
+        category: "Jeans",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 26,
+        name: "Elastic Waist Cargo Pants",
+        price: 45,
+        img: {
+            elastic_waist_black_cargo_pants_one,
+            elastic_waist_black_cargo_pants_two
+        },
+        category: "Pants",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 27,
+        name: "Paris Murky Blue Jeans",
+        price: 45,
+        img: {
+            paris_murky_blue_jeans_one,
+            paris_murky_blue_jeans_two
+        },
+        category: "Jeans",
+        descriptions: "coming soon..."
+    },
+    //shoes
+    {
+        id: 28,
+        name: "Black Faux Leather Sneakers",
+        price: 25,
+        img: {
+            black_brown_sole_sneakers_one,
+            black_brown_sole_sneakers_two,
+            black_brown_sole_sneakers_three
+        },
+        category: "Shoes",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 29,
+        name: "Fuzzy Ankle Boots",
+        price: 55,
+        img: {
+            fuzzy_ankle_boots_one,
+            fuzzy_ankle_boots_two
+        },
+        category: "Shoes",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 30,
+        name: "Mustard Color Loafers",
+        price: 45,
+        img: {
+            mustard_color_loafers_one,
+            mustard_color_loafers_two
+        },
+        category: "Shoes",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 31,
+        name: "Black Luxury Loafers",
+        price: 45,
+        img: {
+            black_loafers_white_soles
+        },
+        category: "Shoes",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 32,
+        name: "Cream-colored N Sneakers",
+        price: 35,
+        img: {
+            creamy_n_sneakers
+        },
+        category: "Shoes",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 33,
+        name: "Dark Brown Sneakers",
+        price: 55,
+        img: {
+            dark_brown_sneakers
+        },
+        category: "Shoes",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 34,
+        name: "Dolumdia Creamy Sneakers",
+        price: 55,
+        img: {
+            dolumdia_sneakers
+        },
+        category: "Shoes",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 35,
+        name: "Double Stripe Sneakers",
+        price: 55,
+        img: {
+            double_stripe_sneakers
+        },
+        category: "Shoes",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 36,
+        name: "Mesh Black Leather Loafers",
+        price: 55,
+        img: {
+            mesh_black_loafers
+        },
+        category: "Shoes",
+        descriptions: "coming soon..."
+    },
+    {
+        id: 37,
+        name: "Mesh Gray Sneakers",
+        price: 55,
+        img: {
+            mesh_gray_sneakers
+        },
+        category: "Shoes",
+        descriptions: "coming soon..."
+    },
+];
+
+export const assets = {
+    logo_no_bg,
+    header_image,
+    shirts_front,
+    shirts_img,
+    tie_image,
+    //
+    logo_black_bg,
+    logo_sheki_bg,
+    //
+    calvin_klein_sweatshirt,
+    calvin_klein_sweatshirt_two,
+    //
+    genuine_leather_dress_shoes_one,
+    half_classic_genuine_leather_dress_shoes,
+    //
+    lv_tracksuit
+}
