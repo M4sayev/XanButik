2268750fd53b705a68d365920106e787d7684153
Elvin Musayev <msayev02@gmail.com>
2024-11-27 17:53:08 +0400

    featured items component done

--- src/components/FeaturedItems/FeaturedItems.jsx
+++ src/components/FeaturedItems/FeaturedItems.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import "./FeaturedItems.css";
 import { favoriteItems } from "../../assets/assets.js";
+import { useInView } from 'react-intersection-observer';
 
 function FeaturedItems() {
+
+  const {ref: titleRef, inView: titleInView} = useInView();
+  const {ref: btnRef, inView: btnInView} = useInView();
+
   return (
     <section className='featured-items'>
         <div className='featured-items-content'>
-          <div className='featured-items-title'>
+          <div ref={titleRef} className={`featured-items-title ${titleInView ? "animate-in" : ""}`}>
               <p className='featured-items-paragraph'>Featured items</p>
               <h1 className='featured-items-heading'>Everyone's favourite</h1>
           </div>
@@ -14,8 +19,9 @@ function FeaturedItems() {
             {
               favoriteItems.map((item, index) => {
                 const {name, img, price} = item;
+                const {ref: gridElement, inView: inView} = useInView();
                 return (
-                  <div key={index}>
+                  <div className={inView ? "animate-in" : ""} ref={gridElement} key={index}>
                     <img src={img} alt={name} />
                     <span className='price-popup'>
                       <p className='item-name'>{name}</p>
@@ -26,7 +32,7 @@ function FeaturedItems() {
               })
             }
           </div>
-          <button className='featured-items-view-more-btn'>view more</button>
+          <button ref={btnRef} className={`featured-items-view-more-btn ${btnInView ? "animate-in" : ""}`}>view more</button>
         </div>
     </section>
   )
