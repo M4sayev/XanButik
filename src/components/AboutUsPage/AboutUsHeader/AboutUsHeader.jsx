2a79f248eb056ed78c038b2511000fda89b640ea
Elvin Musayev <msayev02@gmail.com>
2025-08-23 04:00:37 +0400

    Fix: Add some minor improvements into About Us Header

--- src/components/AboutUsPage/AboutUsHeader/AboutUsHeader.jsx
+++ src/components/AboutUsPage/AboutUsHeader/AboutUsHeader.jsx
@@ -8,18 +8,30 @@ import { StoreContext } from '../../../context/StoreContext';
 
 function AboutUsHeader() {
 
-  const {ref: headerAboutRef, inView: headerAboutInView} = useInView();
+  const {ref: headerAboutRef, inView: headerAboutInView} = useInView({
+    threshold: 0.2,
+    triggerOnce: true
+  });
   const {handleAnimation} = useContext(StoreContext);
 
   return (
-    <header className="header-about-us">
-      <Parallax speed={-15} className="header-parallax-bg header-bg-animate"></Parallax>
+    <header className="header-about-us" aria-label="About Us Intro Section">
+      <Parallax 
+        speed={-15} 
+        className="header-parallax-bg header-bg-animate"
+        aria-hidden="true"
+        role='img'
+      ></Parallax>
         <div ref={headerAboutRef} className={`header-contents-about-us ${handleAnimation(headerAboutInView)}`}>
           <h1 className="about-us-header-title std-heading">Classic Men's Apparel Collection</h1>
           <p className="about-us-header-paragraph std-paragraph">
           Discover our curated collection of classic men's apparel, where timeless elegance meets modern sophistication. From tailored suits to versatile shirts, our selection offers premium quality and impeccable style, ensuring you make a lasting impression with every outfit. Embrace the epitome of refined fashion.
           </p>
-          <Button as={Link} to="/Testimonials" tabIndex="8" className="widget-learn-more-btn std-button">learn more</Button>
+          <Button 
+            as={Link} to="/Testimonials" 
+            className="widget-learn-more-btn std-button"
+            aria-label="Learn more about customer testimonials"
+          >learn more</Button>
         </div>
     </header>
   )
