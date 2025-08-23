d3f6a712cd57766f35778550832c40d96f88e749
Elvin Musayev <msayev02@gmail.com>
2025-08-23 16:35:47 +0400

    Refactor: use map to map days of the weeks make code more readable

--- src/components/ContactUs/Contacts/Contacts.jsx
+++ src/components/ContactUs/Contacts/Contacts.jsx
@@ -9,58 +9,39 @@ function Contacts() {
     const {handleAnimation} = useContext(StoreContext);
 
   return (
-    <section className="contacts-section">
+    <section className="contacts-section"  aria-labelledby="contacts-heading">
+        <h1 id="contacts-heading" className="visually-hidden">Contact Information and Working Hours</h1>
         <div className="contacts-contents">
             <div ref={contactRef} className={`contacts-address-info-container ${handleAnimation(contactInView)}`}>
-                <h1 className="std-heading info-container-heading">Address</h1>
+                <h1 id="contacts-heading" className="std-heading info-container-heading">Address</h1>
                 <p className="std-paragraph mi-auto">We’re excited to meet you! Feel free to get in touch with us within these working hours.</p>
-                <ul className="contact-widgets-container">
-                    <li className="conatct-widget-item">
+                <address>
+                    <ul aria-label="Contact details" className="contact-widgets-container">
+                        <li className="contact-widget-item">
                         <span className="widget-name">Email</span>
-                        <span className="widget-info">msayev02@gmail.com</span>
-                    </li>
-                    <li className="conatct-widget-item">
+                        <a className="widget-info" href="mailto:msayev02@gmail.com">msayev02@gmail.com</a>
+                        </li>
+                        <li className="contact-widget-item">
                         <span className="widget-name">Phone</span>
-                        <span className="widget-info">0554584886</span>
-                    </li>
-                    <li className="conatct-widget-item">
+                        <a className="widget-info" href="tel:0554584886">0554584886</a>
+                        </li>
+                        <li className="contact-widget-item">
                         <span className="widget-name">Address</span>
                         <span className="widget-info">Blah Blah Street, 420</span>
-                    </li>
-                </ul>
+                        </li>
+                    </ul>
+                </address>
             </div>
             <div className="contacts-working-hours-info-container">
                 <div ref={WhRef} className={`working-hours-contents ${handleAnimation(WhInView)}`}>
-                    <h1 className="std-heading info-container-heading">Working Hours</h1>
-                    <ul className="workin-hours-list">
-                        <li className="work-hour-item">
-                            <span className="day-of-the-week">Monday</span>
-                            <span className="hours">09:00 <span>am</span> - 07:30 <span>pm</span></span>
-                        </li>
-                        <li className="work-hour-item">
-                            <span className="day-of-the-week">Tuesday</span>
-                            <span className="hours">09:00 <span>am</span> - 07:30 <span>pm</span></span>
-                        </li>
-                        <li className="work-hour-item">
-                            <span className="day-of-the-week">Wednesday</span>
-                            <span className="hours">09:00 <span>am</span> - 07:30 <span>pm</span></span>
-                        </li>
-                        <li className="work-hour-item">
-                            <span className="day-of-the-week">Thursday</span>
-                            <span className="hours">09:00 <span>am</span> - 07:30 <span>pm</span></span>
-                        </li>
-                        <li className="work-hour-item">
-                            <span className="day-of-the-week">Friday</span>
-                            <span className="hours">09:00 <span>am</span> - 07:30 <span>pm</span></span>
-                        </li>
-                        <li className="work-hour-item">
-                            <span className="day-of-the-week">Saturday</span>
-                            <span className="hours">09:00 <span>am</span> - 07:30 <span>pm</span></span>
-                        </li>
-                        <li className="work-hour-item">
-                            <span className="day-of-the-week">Sunday</span>
-                            <span className="hours">09:00 <span>am</span> - 07:30 <span>pm</span></span>
-                        </li>
+                    <h1 id="contacts-heading" className="std-heading info-container-heading">Working Hours</h1>
+                    <ul aria-label="Working hours details" className="workin-hours-list">
+                        {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map((day) => (
+                            <li className="work-hour-item" key={day}>
+                                <span className="day-of-the-week">{day}</span>
+                                <span className="hours">09:00 <span>am</span> - 07:30 <span>pm</span></span>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
