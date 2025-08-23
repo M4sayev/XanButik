c53cff505bb98368dcc8ebb0277d5007f2da69d8
Elvin Musayev <msayev02@gmail.com>
2025-08-23 15:28:06 +0400

    Feat(A11y): add accessibility TestimonialsOurClients component

--- src/components/TestimonialsPage/TestimonialsOurClients/TestimonialsOurClients.jsx
+++ src/components/TestimonialsPage/TestimonialsOurClients/TestimonialsOurClients.jsx
@@ -8,25 +8,30 @@ function TestimonialsOurClients() {
     const {ref: tOurClientsTextRef, inView: tOurClientsTexInView} = useInView();
     const { handleAnimation } = useContext(StoreContext);
   return (
-    <section className="testimonials-our-clients">
+    <section className="testimonials-our-clients" aria-labelledby='our-clients-heading' role='region'>
         <div className="t-our-client-contents"> 
             <div ref={tOurClientsTextRef} className={`t-our-clients-text-container ${handleAnimation(tOurClientsTexInView)}`}>
-                <h1 className="std-heading">Our clients</h1>
+                <h1 id='our-clients-heading' className="std-heading">Our clients</h1>
                 <p className="std-paragraph mi-auto">Welcome to our clients section - the perfect place for fashionably-minded men everywhere! Here you can explore an array of stylish clothes that will update your wardrobe. Whether you're looking for a classic staple or something trendier, there's something here just for you.</p>
             </div>
-            <div className="t-our-results-grid">
+            <ul className="t-our-results-grid" role="list">
                 {
                     ourBrandCompanies.map((clientItem, index) => {
                         const {ref: clientItemRef, inView: clientItemInView} = useInView();
                         const {img, brandName} = clientItem;
                         return (
-                            <div ref={clientItemRef} key={index} className={`client-item-container ${handleAnimation(clientItemInView)}`}>
-                                <img src={img} alt={brandName} />
+                            <div 
+                                ref={clientItemRef} 
+                                key={index} 
+                                className={`client-item-container ${handleAnimation(clientItemInView)}`}
+                                aria-label={`Logo of ${brandName}`}
+                            >
+                                <img src={img} aria-hidden="true"/>
                             </div>
                         )
                     })
                 }
-            </div>
+            </ul>
         </div>
     </section>
   )
