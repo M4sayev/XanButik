88ac64d6eebd59399718690c8e7978c5a3e41e0f
Elvin Musayev <msayev02@gmail.com>
2025-08-24 13:29:56 +0400

    Refactor: clean up Contacts components add some accessibility

--- src/components/ContactUs/Contacts/Contacts.jsx
+++ src/components/ContactUs/Contacts/Contacts.jsx
@@ -3,6 +3,17 @@ import "./Contacts.css";
 import { useInView } from 'react-intersection-observer';
 import {StoreContext} from "../../../context/StoreContext";
 
+const workingHours = {
+  Monday: ['09:00', '19:30'],
+  Tuesday: ['09:00', '19:30'],
+  Wednesday: ['09:00', '19:30'],
+  Thursday: ['09:00', '19:30'],
+  Friday: ['09:00', '19:30'],
+  Saturday: ['09:00', '19:30'],
+  Sunday: ['09:00', '19:30'],
+};
+
+
 function Contacts() {
     const {ref: contactRef, inView: contactInView} = useInView();
     const {ref: WhRef, inView: WhInView} = useInView();
@@ -33,13 +44,25 @@ function Contacts() {
                 </address>
             </div>
             <div className="contacts-working-hours-info-container">
-                <div ref={WhRef} className={`working-hours-contents ${handleAnimation(WhInView)}`}>
-                    <h1 id="contacts-heading" className="std-heading info-container-heading">Working Hours</h1>
-                    <ul aria-label="Working hours details" className="workin-hours-list">
-                        {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map((day) => (
+                <div 
+                    ref={WhRef} 
+                    className={`working-hours-contents ${handleAnimation(WhInView)}`}
+                    aria-labelledby="working-hours-heading"
+                >
+                    <h1 id="working-hours-heading" className="std-heading info-container-heading">Working Hours</h1>
+                    <ul  aria-label="Working hours for each day of the week" className="workin-hours-list">
+                        {Object.entries(workingHours).map(([day, hours]) => (
                             <li className="work-hour-item" key={day}>
                                 <span className="day-of-the-week">{day}</span>
-                                <span className="hours">09:00 <span>am</span> - 07:30 <span>pm</span></span>
+                                <span className="hours">
+                                    <time datetime={hours[0]}>
+                                        {hours[0]} <span>am</span>
+                                    </time>
+                                    -
+                                    <time datetime={hours[1]}>
+                                        {hours[1]} <span>pm</span>
+                                    </time>
+                                </span>
                             </li>
                         ))}
                     </ul>
