47bf6e261235d023c54694ad21abe72c27f91d0d
Elvin Musayev <msayev02@gmail.com>
2025-09-27 23:58:10 +0400

    Feat: add order summary component

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
 import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
+
+import OrderSummary from "../../components/Cart/OrderSummary/OrderSummary";
 
 function Cart() {
+  const { cartItems } = useContext(StoreContext);
+
   return (
     <main>
-      <div className="Order Summary">
-        <h1></h1>
+      <div className="cart-contents">
+        <OrderSummary cartItems={cartItems} />
       </div>
     </main>
   );
