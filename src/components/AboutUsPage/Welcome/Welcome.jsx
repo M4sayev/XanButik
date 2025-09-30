781a169a9f6d4b63e829748a364222b1809a161c
Elvin Musayev <msayev02@gmail.com>
2025-09-30 22:01:19 +0400

    Fix: image size corrections

--- src/components/AboutUsPage/Welcome/Welcome.jsx
+++ src/components/AboutUsPage/Welcome/Welcome.jsx
@@ -7,22 +7,34 @@ import { handleAnimation } from "../../../utils/utils";
 function Welcome() {
   const { ref: welcomeTextAreaRef, inView: welcomeTextInView } = useInView({
     threshold: 0.2,
-    triggerOnce: true
+    triggerOnce: true,
   });
   const { ref: welcomeImgRef, inView: welcomeImgInView } = useInView({
     threshold: 0.2,
-    triggerOnce: true
+    triggerOnce: true,
   });
 
   return (
-    <section className="about-us-welcome-section" aria-labelledby="welcome-heading">
+    <section
+      className="about-us-welcome-section"
+      aria-labelledby="welcome-heading"
+    >
       <div className="welcome-section-contents">
         <article
           ref={welcomeTextAreaRef}
-          className={`welcome-section-text ${handleAnimation(welcomeTextInView)}`}
+          className={`welcome-section-text ${handleAnimation(
+            welcomeTextInView
+          )}`}
         >
-          <p className="welcome-introductory-title std-paragraph std-subtitle-fs mi-auto">about us</p>
-          <h1 className="welcome-section-text-title std-heading" id="welcome-heading">Welcome</h1>
+          <p className="welcome-introductory-title std-paragraph std-subtitle-fs mi-auto">
+            about us
+          </p>
+          <h1
+            className="welcome-section-text-title std-heading"
+            id="welcome-heading"
+          >
+            Welcome
+          </h1>
           <p className="welcome-main-text-paragraph std-paragraph">
             Discover Xan Butik, your go-to destination for men's fashion in
             Sheki, Azerbaijan. With a commitment to affordable prices, loyalty
@@ -31,13 +43,17 @@ function Welcome() {
             quality fashion at Xan Butik.
           </p>
         </article>
-        <img
-          ref={welcomeImgRef}
-          className={`welcome-section-img ${handleAnimation(welcomeImgInView)}`}
-          src={assets.logo_sheki_bg}
-          alt="Xan Butik logo"
-          loading="lazy"
-        />
+        <div className="welcome-img-wrapper">
+          <img
+            ref={welcomeImgRef}
+            className={`welcome-section-img ${handleAnimation(
+              welcomeImgInView
+            )}`}
+            src={assets.logo_sheki_bg}
+            alt="Xan Butik logo"
+            loading="lazy"
+          />
+        </div>
       </div>
     </section>
   );
