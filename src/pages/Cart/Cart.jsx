b1bbdc74b8ccd594a05796c10e7630d97c1dffd3
Elvin Musayev <msayev02@gmail.com>
2025-09-30 20:09:05 +0400

    Fix in the pathname

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext.jsx";
 import OrderSummary from "../../components/Cart/OrderSummary/OrderSummary.jsx";
 import CartItemsList from "../../components/Cart/CartItemsList/CartItemsList.jsx";
-import EmptyCartMessage from "../../components/Cart/EmptyCart/EmptyCartMEssage.jsx";
+import EmptyCartMessage from "../../components/Cart/EmptyCart/EmptyCartMessage.jsx";
 
 function Cart() {
   const { cartItems, setCartItems } = useContext(StoreContext);
