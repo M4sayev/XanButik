9198a3d9606ae8b920ff2d129ed35c353d249a3e
Elvin Musayev <msayev02@gmail.com>
2025-10-10 23:16:32 +0400

    Refactor: create one GamScreens css for both StartMenu and GameFinished

--- src/components/GamePage/StartMenu/StartMenu.jsx
+++ src/components/GamePage/StartMenu/StartMenu.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import PlayButton from "../PlayButton/PlayButton";
 import { LuTrophy } from "react-icons/lu";
 import backgroundImage from "../../../assets/game/game_bg.png";
-import "./StartMenu.css";
+import "../CurrentGamePage/GameScreens.css";
 
 function StartMenu({ setStartTime }) {
   return (
     <div
-      className="game-start-menu-container"
+      className="game-screen-container"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="game-contents-overlay"></div>
-      <div className="game-start-menu">
+      <div className="game-screen">
         <LuTrophy aria-hidden="true" className="trophy-icon" />
         <h1 className="std-heading">The ultimate tapping challenge</h1>
         <p className="std-paragraph mi-auto">
