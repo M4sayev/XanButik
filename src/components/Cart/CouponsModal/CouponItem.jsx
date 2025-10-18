2667f77a1bbddea1fdb12d1aa609e6dea6b5dfa9
Elvin Musayev <msayev02@gmail.com>
2025-10-18 20:32:42 +0400

    Feat: dynamically display coupons design according to the coupons bought

--- src/components/Cart/CouponsModal/CouponItem.jsx
+++ src/components/Cart/CouponsModal/CouponItem.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "./CouponItem.css";
 
-function CouponItem(theme = "silver") {
+function CouponItem({ price, offer }) {
   return (
     <button
       className="coupon-item"
       style={
-        theme === "silver"
+        price.coinValue === "silver"
           ? { background: "var(--gradient-silver)" }
           : { background: "var(--gradient-gold)" }
       }
     >
       <div className="item-info-wrapper">
         <div className="coupon-item-info">
-          <p className="coupon-offer-value">25%</p>
-          <span className="coupon-bg-text">25</span>
-          <p className="coupon-offer-text">OFF</p>
+          <p className="coupon-offer-value">{offer.value}</p>
+          <span className="coupon-bg-text">{offer.value}</span>
+          <p className="coupon-offer-text">{offer.text}</p>
         </div>
       </div>
       <div className="coupon-item-removable-part"></div>
